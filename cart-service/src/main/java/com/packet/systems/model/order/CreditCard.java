d4c59642e225150370c768a7545ffef2a5f8cf5a
Rudiansen <rudiansen.gunawan@packet-systems.com>
2020-12-21 14:32:20 +0700

    change CreditCard model

--- cart-service/src/main/java/com/packet/systems/model/order/CreditCard.java
+++ cart-service/src/main/java/com/packet/systems/model/order/CreditCard.java
@@ -9,8 +9,8 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class CreditCard {
 
-    private String name;
     private String number;
+    private String expiration;
     private String nameOnCard;
 
 }
